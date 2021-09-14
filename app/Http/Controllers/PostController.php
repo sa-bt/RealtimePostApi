2421a8e159d55b75eb4c446d9948ef7e1426704c
sa-bt <bakhshian2020@gmail.com>
2021-09-14 18:36:39 +0430

    create post and get all post

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->only('store');
+    }
+
+    public function index()
+    {
+        $posts=Post::with('user')->latest()->get();
+        return PostResource::collection($posts);
+    }
+
+    public function store(Request $request)
+    {
+       $this->validate($request, [
+            'body' => ['required']
+        ]);
+        $post=$request->user()->posts()->create($request->only('body'));
+        return new PostResource($post);
+    }
+}
